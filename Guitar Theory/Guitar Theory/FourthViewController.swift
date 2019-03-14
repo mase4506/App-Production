2a24134f2331856fbf15b895dd5bf8ebde67aecb
mase4506 <47620212+mase4506@users.noreply.github.com>
2019-03-13 20:51:36 -0600

    Add files via upload

---
+++ Guitar Theory/Guitar Theory/FourthViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FourthViewController.swift
+//  Guitar Theory
+//
+//  Created by Matt Sexton on 3/10/19.
+//  Copyright © 2019 Matt Sexton. All rights reserved.
+//
+
+import UIKit
+
+class FourthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Guitar Theory/Guitar Theory/TabBarViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TabBarViewController.swift
+//  Guitar Theory
+//
+//  Created by Matt Sexton on 3/10/19.
+//  Copyright © 2019 Matt Sexton. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillLayoutSubviews() {
+        var tabFrame = self.tabBar.frame
+        // - 40 is editable , the default value is 49 px, below lowers the tabbar and above increases the tab bar size
+        tabFrame.size.height = 100
+        tabFrame.origin.y = self.view.frame.size.height - 100
+        self.tabBar.frame = tabFrame
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
